141e0cec02ccc546c32c0643b9cff74c9702c249
Ricardo Serin <serin.ricardo@gmail.com>
2020-10-04 12:20:40 -0500

    casi listo

--- winproySerialPort/winproySerialPort/Form2.Designer.cs
+++ winproySerialPort/winproySerialPort/Form2.Designer.cs
@@ -63,6 +63,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(48, 48);
             this.button1.TabIndex = 0;
+            this.button1.Text = "}";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
